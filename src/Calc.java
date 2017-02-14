773939bd74f0a5ecc8ef801cb48f07df9ca0d6c9
Dima Zherebko <kraken@live.ru>
2017-02-14 13:45:18 +0200

    Conflict with alosha test

--- src/Calc.java
+++ src/Calc.java
@@ -23,11 +23,11 @@ public class Calc {
        s=s.replaceAll(" ","");
         if (Utils.isDigit(s)) {
             double digit = Double.parseDouble(s);
-            //System.out.println(digit);
+            //Sfdsafsadfgdsaf
             return digit;
         } else {
             Expr e = new Expr(s);
-           // System.out.println(e);
+           // System.out.println(abcdef);
             double left = getDigit(e.experssion.left);
             double right = getDigit(e.experssion.right);
 
@@ -64,9 +64,7 @@ public class Calc {
         return res;
 
     }
-  static   double pow(double n){
+    static   double pow(double n){
         return ((double)((int)(n*100)))/100;
     }
-
-
 }
